7436726403612fc903bec3567580145849c501bd
Firdevs Çolak <firdevscolak81@gmail.com>
2025-07-30 23:39:24 +0300

    dataset

--- code/dataset.py
+++ code/dataset.py
@@ -1,20 +1,4 @@
-'''
-problem tanimi: gemini ile aisstant projesi
--kullanicinin dogal dilde verdigi komutlari anlar
--kural tabanli olarak notlar olcak
--bu chatbot notlara erisecek, özet verecek
+import sqlite3
+import os
 
-model tanimi: Google gemini 
--gemini-2.0-flash
-
-api tanimi: AIzaSyDuc28TxN32U3Kb0gVKoBqGf8bBkim9Ta4
-
-plan program:
--assistant: chatbot 
--dataset:sqlite database 
--main: b
-
-pip install requests python-dotenv
-pip freeze > requirements.txt
-
-'''
+DB_PATH = os.path.join("data","assistant.db")
