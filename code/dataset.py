0ead14b4164f56623453bf9de8c60bfe0a8ebaa8
Firdevs Çolak <firdevscolak81@gmail.com>
2025-07-31 22:42:26 +0300

    sql

--- code/dataset.py
+++ code/dataset.py
@@ -1,4 +1,81 @@
 import sqlite3
 import os
 
-DB_PATH = os.path.join("data","assistant.db")
+#veritabani dosyasi yolu data/assistand.db
+DB_PATH = os.path.join("data","assistant.db") #env icine yazilmasi daha dogru
+
+#database initializer
+def initialize_db():
+    os.makedirs("data", exist_ok=True)
+    
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS notes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            content TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS calendar(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            event TEXT NOT NULL,
+            event_date TEXT NOT NULL
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def add_note(content):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("INSERT INTO notes (content) VALUES (?)", (content,))
+    
+    conn.commit()
+    conn.close()
+
+def add_event(event, event_date):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("INSERT INTO calendar (event, event_date) VALUES (?, ?)", (event, event_date))
+   
+    conn.commit()
+    conn.close()
+
+def get_notes():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT content, created_at FROM notes ORDER BY created_at DESC")
+    notes = cursor.fetchall()
+    
+    conn.close()
+    return notes
+
+def get_events():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT event, event_date FROM calendar ORDER BY event_date DESC")
+    
+    events = cursor.fetchall()
+    
+    conn.close()
+    return events
+    
+    
+if __name__ == "__main__":
+    initialize_db() #extensions-> SQLite Viewer
+    add_note("su iç oh")
+    add_event("toplanti var", "20.00da")
+    
+    print(f"NOtes: {get_notes()}")
+    print(f"Events:{get_events()}")
+    
+    
---
+++ code/main.py
@@ -0,0 +1,20 @@
+'''
+problem tanimi: gemini ile aisstant projesi
+-kullanicinin dogal dilde verdigi komutlari anlar
+-kural tabanli olarak notlar olcak
+-bu chatbot notlara erisecek, özet verecek
+
+model tanimi: Google gemini 
+-gemini-2.0-flash
+
+api tanimi: AIzaSyDuc28TxN32U3Kb0gVKoBqGf8bBkim9Ta4
+
+plan program:
+-assistant: chatbot 
+-dataset:sqlite database 
+-main: b
+
+pip install requests python-dotenv
+pip freeze > requirements.txt
+
+'''
