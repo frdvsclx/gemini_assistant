9f5fb40ffaa81262b52e3cb6070fe622deb401c8
Firdevs Çolak <firdevscolak81@gmail.com>
2025-08-01 23:31:31 +0300

    gemini

---
+++ code/assistant.py
@@ -0,0 +1,47 @@
+import os
+import requests #http istekleri icin
+from dotenv import load_dotenv #.env dosyasindan ortam varb yuklemek icin
+
+load_dotenv() #.env 
+
+#.env dosyasindan api key al
+api_key = os.getenv("GEMINI_API_KEY")
+
+#api key yoksa hata ver
+if not api_key:
+    raise ValueError("GEMINI_API_KEY not found in .env file")
+
+url ="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+
+headers = {"Content-Type": "application/json",  #json formatinda gonder
+           "X-Google-API-Key": api_key } #yetkilndirme icin 
+
+def get_gemini_response(promt: str) -> str: #ggemini ana fonksiyon
+    payload = {
+        "contents": [
+            { "parts": 
+                [
+                { "text": promt} #kullanici sorusu
+                ]
+            }
+        ]
+    }
+        
+    #geminiye api istegi gonder
+    response = requests.post(url, headers=headers,json=payload)
+        
+    #istek basarili(http 200) ise cevabi dondur
+    if response.status_code == 200:
+        try:
+            result = response.json() #json formatindaki yaniti dic cevir
+            return result["candidates"][0]["content"]["parts"][0]["text"]
+        except Exception as exp:
+            return f"Hata: {exp}"
+    else:
+        return f"api hatasi: {response.status_code}: {response.text}"
+
+if __name__ == "__main__":
+    user_input = input("soru: ") #terminalden soru alma
+    response = get_gemini_response(user_input) #geminiden alinan yanit return edilir
+    print("gemini: ", response) 
+
